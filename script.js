9d460dfa517d5063cc441c79b309621c8de535d1
hr07 <haronthe1@gmail.com>
2025-05-05 19:45:33 +0300

    change 2

---
+++ script.js
@@ -0,0 +1,19 @@
+<script>
+  function showPopup() {
+    const phone = document.getElementById('phoneNumber').value || "0114272741";
+    const message = `QF45T13XY Confirmed. Ksh500.00 sent to Ian Mbalasi ${phone} on 02/05/2025 at 17:14 PM.<br>
+      New M-PESA balance is Ksh3,500.00.<br>
+      Transaction cost, Ksh6.00.<br>
+      Pay with M-PESA Global or Lipa na M-PESA.`;
+
+    const popup = document.getElementById('mpesaPopup');
+    document.getElementById('mpesaMessageText').innerHTML = message;
+
+    popup.style.display = 'block';
+
+    // Automatically hide after 8 seconds (optional if using animation)
+    setTimeout(() => {
+      popup.style.display = 'none';
+    }, 8000);
+  }
+</script>
